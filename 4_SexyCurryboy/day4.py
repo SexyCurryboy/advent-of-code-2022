7a75e436dcaca03a07d46f88b92179dbd4e88dda
SexyCurryboy <46899915+SexyCurryboy@users.noreply.github.com>
2022-12-05 14:37:47 +0100

    Updated solution for day 4

--- 4_SexyCurryboy/day4.py
+++ 4_SexyCurryboy/day4.py
@@ -14,16 +14,11 @@ counter = 0
 for pair in pairs:
     if int(pair[0]) <= int(pair[2]) and int(pair[1]) >= int(pair[3]) or int(pair[0]) >= int(pair[2]) and int(pair[1]) <= int(pair[3]):
         counter += 1
-
 print(counter)
 
 #### Part 2 ####
 counter = 0
 for pair in pairs:
-    if int(pair[0]) <= int(pair[2]) <= int(pair[1]):
-        counter += 1
-    elif int(pair[0]) <= int(pair[3]) <= int(pair[1]):
-        counter += 1
-    elif int(pair[2]) <= int(pair[0]) <= int(pair[3]):
+    if int(pair[0]) <= int(pair[2]) <= int(pair[1]) or int(pair[0]) <= int(pair[3]) <= int(pair[1]) or int(pair[2]) <= int(pair[0]) <= int(pair[3]):
         counter += 1
 print(counter)
