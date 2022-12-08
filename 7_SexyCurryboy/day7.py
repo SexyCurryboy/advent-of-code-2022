6d412edbc5ff1d44d049bd4ec4360e1016b5850b
SexyCurryboy <46899915+SexyCurryboy@users.noreply.github.com>
2022-12-08 17:05:13 +0100

    Task 1 solved

--- 7_SexyCurryboy/day7.py
+++ 7_SexyCurryboy/day7.py
@@ -5,27 +5,23 @@ dir = ""
 path = []
 counter = 0
 
+#### Part 1 ####
 with open("input.txt", "r") as f:
     for line in f:
         if line.startswith("$ cd"):
             dir = line.split()[2]
             if dir == "..":
-                structure[path[-1]] += size
-                structure[path[-2]] = structure[path[-2]] + structure[path[-1]]
                 path.pop()
-                size = 0
             else:
-                size = 0
                 if dir in structure:
                     dir += str(random.randint(1, 999999))
                 path.append(dir)
         elif line[0].isdigit(): #loop until doesn't start with digit or dir
-            size += int(line.split()[0])
-        elif line.startswith("$"): #not listing anymore
-            structure[dir] = size
-
-print(structure)
-
+            for directory in path:
+                if directory in structure:
+                    structure[directory] += int(line.split()[0])
+                else:
+                    structure[directory] = int(line.split()[0])
 
 for size in structure.values():
     if size <= 100000:
